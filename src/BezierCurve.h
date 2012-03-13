d16577ad08120c6c6b14cd4b29ea5fc03d11d1e1
Victor Sand <victor.sand@gmail.com>
2012-03-12 20:40:49 -0700

    bezier

---
+++ src/BezierCurve.h
@@ -0,0 +1,18 @@
+#ifndef BEZIERCURVE_H
+#define BEZIERCURVE_H
+
+#include "MathEngine.h"
+#include <vector>
+
+class BezierCurve {
+public:
+    static Vector3 evaluate(Vector3 _p1, 
+                            Vector3 _p2, 
+                            Vector3 _p3,
+                            float _t);
+    BezierCurve(Vector3 _p1, Vector3 _p2, Vector3 _p3);
+    Vector3 evaluate(float _t);
+    Vector3 p1, p2, p3;
+};
+
+#endif
